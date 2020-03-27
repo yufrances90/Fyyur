1efade3866085c8fa76329ad5c56b6f5e648e0f1
yufrances90 <yufrances90@gmail.com>
2020-03-26 20:21:31 -0700

    feat: Rewrote endpoint for adding new show

--- app/app.py
+++ app/app.py
@@ -505,14 +505,26 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  try:
+    
+    show = Show(
+      artist_id = request.form['artist_id'],
+      venue_id = request.form['venue_id'],
+      start_time = request.form['start_time']
+    )
+
+    db.session.add(show)
+
+    db.session.commit()
+
+    # on successful db insert, flash success
+    flash('Show was successfully listed!')
+
+  except:
+
+    flash('An error occurred. Show could not be listed.')
+
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
